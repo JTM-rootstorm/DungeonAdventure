6b945ebec2f0a407649909cecf0208d6d012ce08
TehGuy <iamtehguy@gmail.com>
2016-01-29 21:36:43 -0600

    removed unneeded includes

--- Engine/Items/Player/HealingPotion.cs
+++ Engine/Items/Player/HealingPotion.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Engine.Items;
-
-namespace Engine.Items.Player
+﻿namespace Engine.Items.Player
 {
     public class HealingPotion : Item
     {
